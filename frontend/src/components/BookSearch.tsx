88c75462fe37b53c05a7480b65528a4c4f7282a8
nemethrami <nemethramona35@gmail.com>
2024-10-20 17:58:16 +0200

    feat: use google books api from react component

--- frontend/src/components/BookSearch.tsx
+++ frontend/src/components/BookSearch.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState, BaseSyntheticEvent, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { BookModel } from '../models/BookModel';
 import edition_placeholder from '../assets/edition_placeholder.png'
 import ImageList from '@mui/material/ImageList';
@@ -10,41 +10,46 @@ import InfoIcon from '@mui/icons-material/Info';
 import "./BookSearch.css";
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
-import Box from '@mui/material/Box';
 
 function BookSearch() {
-
-    const [book, setBook] = useState("");
-    const [result, setResult] = useState([]);
+    const [book, setBook] = useState<string>('');
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
     const linkRef = useRef<HTMLAnchorElement>(null);
+    const API_KEY = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
 
-    async function getBooksByName(book: string) {
-        try {
-            const response = await axios.get(`http://localhost:8000/books_by_name?book=${book}`);
-            return response.data;
-        } catch (error) {
-            console.error('Error fetching books: ', error);
-            throw error;
-        }
-        
-    }
-
-    function handleChange(event: BaseSyntheticEvent) {
-        const book = event.target.value;
-        setBook(book);
-    }
-
-    function handleSubmit(event: BaseSyntheticEvent) {
+    const fetchBooks = async (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
 
-        getBooksByName(book)
-        .then((data) => {
-            setResult(data);
-        })
-        .catch((error) => {
-            console.error('Error: ', error)
-        })
-    }
+        if (!book.trim()) {
+            // Do not proceed if the search query is an empty string
+            setBooks([]);
+            return;
+        }
+
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await axios.get(
+                'https://www.googleapis.com/books/v1/volumes',
+                {
+                    params: {
+                        q: book,
+                        key: API_KEY,
+                        maxResults: 40,
+                    },
+                }
+            );
+
+            setBooks(response.data.items || []);
+        } catch (err) {
+            setError(err.response?.data?.error?.message || 'Failed to fetch books');
+        } finally {
+            setLoading(false);
+        }
+    };
 
     function openNewWindow(previewLink: string) {
         window.open(previewLink, '_blank');
@@ -52,55 +57,44 @@ function BookSearch() {
 
     return (
         <>
-         <Box 
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: '100%', // 100% szélesség
-                    maxWidth: '280px', // Maximális szélesség
-                    margin: '0 auto', // Középre igazítás
-                    border: '1px solid #ccc',
-                    borderRadius: '4px',
-                    padding: '4px',
-                }}
-            >
-                <form onSubmit={handleSubmit} className='form'>
-                    <InputBase
-                        onChange={handleChange}
-                        sx={{ flex: 1}}
-                        placeholder="Keresés"
-                        inputProps={{ 'aria-label': 'search books' }}
-                        autoComplete='off'
-                    />
-                    <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
-                        <SearchIcon />
-                    </IconButton>
-                </form>
-            </Box>
-            <ImageList sx={{ width: '100%', height: 650 }} cols={8}>
-                {result.map((book: BookModel) => (
-                    <ImageListItem key={book.volumeInfo.imageLinks?.thumbnail || edition_placeholder}>
-                    <img
-                        srcSet={`${book.volumeInfo.imageLinks?.thumbnail || edition_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                        src={`${book.volumeInfo.imageLinks?.thumbnail || edition_placeholder}?w=248&fit=crop&auto=format`}
-                        alt={book.volumeInfo.title}
-                        loading="lazy"
-                    />
-                    <ImageListItemBar
-                        title={book.volumeInfo.title}
-                        subtitle={book.volumeInfo.authors}
-                        actionIcon={
-                            <a href='#' ref={linkRef} onClick={() => openNewWindow(book.volumeInfo.previewLink)}>
-                                <IconButton
-                                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                                    aria-label={`info about ${book.volumeInfo.title}`}
-                                >
-                                    <InfoIcon />
-                                </IconButton>
-                            </a>
-                        }
-                    />
+            <form className='form'>
+                <InputBase
+                    onChange={(e) => setBook(e.target.value)}
+                    sx={{ flex: 1}}
+                    placeholder="Search for books"
+                    inputProps={{ 'aria-label': 'search books' }}
+                    autoComplete='off'
+                />
+                <IconButton type="submit" sx={{ p: '10px' }} aria-label="search" onClick={(e) => fetchBooks(e)}>
+                    <SearchIcon />
+                </IconButton>
+            </form>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            <ImageList sx={{ width: '100%', height: '80vh', margin: '0', padding: '0' }} cols={8}>
+                {books.map((book: BookModel) => (
+                    <ImageListItem key={book.volumeInfo.imageLinks?.thumbnail.replace('http://', 'https://') || edition_placeholder}>
+                        <img
+                            srcSet={`${book.volumeInfo.imageLinks?.thumbnail.replace('http://', 'https://') || edition_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                            src={`${book.volumeInfo.imageLinks?.thumbnail.replace('http://', 'https://') || edition_placeholder}?w=248&fit=crop&auto=format`}
+                            alt={book.volumeInfo.title}
+                            loading="lazy"
+                        />
+                        <ImageListItemBar
+                            title={book.volumeInfo.title}
+                            subtitle={book.volumeInfo.authors}
+                            actionIcon={
+                                <a href='#' ref={linkRef}>
+                                    <IconButton
+                                        sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                        aria-label={`info about ${book.volumeInfo.title}`}
+                                        onClick={() => openNewWindow(book.volumeInfo.previewLink)}
+                                    >
+                                        <InfoIcon />
+                                    </IconButton>
+                                </a>
+                            }
+                        />
                     </ImageListItem>
                 ))}
             </ImageList>
